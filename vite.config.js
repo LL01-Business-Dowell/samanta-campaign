29ae0bd47dcb158d44b6d9f6080428390b8ba70a
Manish <mdashsharma95@gmail.com>
2024-12-28 16:54:36 +0530

    added docker

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8005,
+  },
   plugins: [react()],
+  base: '/samantacampaign/'
 })
